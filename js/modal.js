182bb96815a7daf0513b16c794b2a3e37c8f2b18
MikeRodnoy <nurmamed0ff07@gmail.com>
2022-04-22 21:59:43 +0300

    .

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,18 @@
-(() => {
+
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+    openModalBtn: document.querySelector('.data-modal-open'),
+    closeModalBtn: document.querySelector('.data-modal-close'),
+    modal: document.querySelector('.data-modal'),
+};
+  
+// console.log(refs.modal);
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+  console.log('clicked');
+  }
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+  
